467843f2b302e778631d4e010ef2b513f2f6d13f
mremmalex <mremmalex2@gmail.com>
2022-06-17 13:25:29 +0100

    github intro

---
+++ forloop.py
@@ -0,0 +1,65 @@
+# BankNames = [
+#     "firstbank",
+#     "accessbank",
+#     "unionbank"
+# ]
+
+# accountBalance = 10000
+# newBalance = 0
+
+
+# def withdraw(atmCard, amount):
+#     if atmCard == "true":
+#         if accountBalance >= amount:
+#             newBalance = accountBalance - amount
+#             return f"withdraw success, {newBalance}"
+#         else:
+#             return "insufficient Fund"
+#     else:
+#         return
+
+
+# def transfer(atmCard, amount, bankName, accountNumber):
+#     if atmCard == "true":
+#         if bankName in BankNames:
+#             if accountBalance >= amount:
+#                 accountBalance - amount
+#                 accountNumber + amount
+#                 return "transfer success"
+#             else:
+#                 return "insufficient Fund"
+#         else:
+#             return "invalid bank name"
+
+
+# def balance(atmCard):
+#     if atmCard == "true":
+#         return newBalance
+#     else:
+#         return
+
+
+# for _ in range(3):
+#     atmCard = input("Enter Your Atm Card: ")
+#     transaction = input("Transaction Type: ")
+
+#     if transaction == "transfer":
+#         transfer(atmCard, 1000, "firstbank", 2343567456)
+#         # print(output)
+#     elif transaction == "withdraw":
+#         output = withdraw(atmCard, 2000)
+#         print(output)
+#     elif transaction == "balance":
+#         output = balance(atmCard)
+#         print(output)
+
+
+count = 10
+
+while True:
+    # count += 1
+    print("hello")
+
+
+# https://medium.com/devops-with-valentine/2021-how-to-set-up-your-ssh-key-for-github-on-windows-10-afe6e729a3c0
+# https://git-scm.com/downloads
